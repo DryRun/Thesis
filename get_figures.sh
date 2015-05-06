e6212a6160c229e78a761a43ac6a4681db3ac854
DryRun <david.renhwa.yu@gmail.com>
2015-05-05 18:35:18 -0700

    Add auxiliary stuff

---
+++ get_figures.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/onZ/DY_TauPt.pdf figures/ch5-model-independent/inttau_onZ_DY_TauPt.pdf
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/onZ/DY_TauEta.pdf figures/ch5-model-independent/inttau_onZ_DY_TauEta.pdf
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/offZ_OSSF/DY_TauPt.pdf figures/ch5-model-independent/inttau_offZ_OSSF_DY_TauPt.pdf
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/offZ_OSSF/DY_TauEta.pdf figures/ch5-model-independent/inttau_offZ_OSSF_DY_TauEta.pdf
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/offZ_mixed/DY_TauPt.pdf figures/ch5-model-independent/inttau_offZ_mixed_DY_TauPt.pdf
+cp ../Multilepton/Documents/ML2012InternalNote/figures/TauCR/offZ_mixed/DY_TauEta.pdf figures/ch5-model-independent/inttau_offZ_mixed_DY_TauEta.pdf
