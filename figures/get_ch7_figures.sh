e86284b1ab9d2e74f1c58709afa31a88db7e28ae
DryRun <david.renhwa.yu@gmail.com>
2015-05-27 12:23:41 -0700

    Major reorganizaiton

---
+++ figures/get_ch7_figures.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/FeynmanDiagrams/fd_cc2.eps* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/FeynmanDiagrams/fd_cn2.eps* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/c_br_charged* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/c_br_neutral* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/c_xsec_t3ss.png* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/vll_xsec.png* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/c_DeltaM_vs_DeltaR_Ze* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CR/c_output_dR_Ze_InclusiveNoM3LDeltaR_300GeV_log* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/DeltaR/c_limits_dRVariation_VLL_Combined_BRe1.0_BRmu0.0*.pdf* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/DeltaR/c_limits_dRVariation_Seesaw_Combined_BRe1.0_BRmu0.0*.pdf* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/c_oppositeside_charged.pdf* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/c_oppositeside_neutral.pdf* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/c_eff_fid_Ze_Seesaw* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/c_eff_fid_Ze_VLL* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/c_eff_fid_Zmu_Seesaw* ch7-resonance/
+cp /Users/dryu/Documents/Physics/ATLAS/Multilepton/Documents/TypeIIISeesaw8TeV/figures/david/CutOptimization/c_eff_fid_Zmu_VLL* ch7-resonance/
